c45a005a9c5ce93e6391c4db7955f615a5dfaf1e
Bubnova Violetta <vveta09@gmail.com>
2018-11-03 19:04:29 +0300

    ExoPlayer

---
+++ radio/app/src/main/java/radioapp/com/radio/PlaybackStatus.java
@@ -0,0 +1,15 @@
+package radioapp.com.radio;
+
+public class PlaybackStatus {
+
+    public static final String IDLE = "PlaybackStatus_IDLE";
+
+    public static final String LOADING = "PlaybackStatus_LOADING";
+
+    public static final String PLAYING = "PlaybackStatus_PLAYING";
+
+    public static final String PAUSED = "PlaybackStatus_PAUSED";
+
+    public static final String STOPPED = "PlaybackStatus_STOPPED";
+
+}
