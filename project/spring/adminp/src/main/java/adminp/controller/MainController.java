60d71e376f2475c37020bbf4acee8b8e72f5f664
Bubnova Violetta <vveta09@gmail.com>
2019-01-13 09:52:26 +0300

    -

--- project/spring/adminp/src/main/java/adminp/controller/MainController.java
+++ project/spring/adminp/src/main/java/adminp/controller/MainController.java
@@ -10,9 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import java.io.IOException;
 import java.util.Map;
-
 @Controller
 public class MainController {
     @Autowired
@@ -24,10 +22,10 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
+    public String main(Model model) {
         Iterable<Message> messages = messageRepo.findAll();
 
-        model.put("messages", messages);
+        model.addAttribute("messages", messages);
 
         return "main";
     }
@@ -43,21 +41,6 @@ public class MainController {
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
-
-        model.put("messages", messages);
-
-        return "main";
-    }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
-
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        } else {
-            messages = messageRepo.findAll();
-        }
 
         model.put("messages", messages);
 
